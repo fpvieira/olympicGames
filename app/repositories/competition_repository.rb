1ab20fc79db8a8f9e73da661ec17840b994300b7
Felipe Vieira <fpvieira@gmail.com>
2018-11-10 09:11:35 -0200

    Cria repositories

---
+++ app/repositories/competition_repository.rb
@@ -0,0 +1,29 @@
+class CompetitionRepository
+  include ActiveModel::Validations
+  
+  def initialize(discipline_slug)
+    discipline_repository = DisciplineRepository.new
+    @discipline = discipline_repository.find_by_slug(discipline_slug)
+  end 
+
+  def get_by_id(id)
+    Competition.find_by!(id: id)
+  end
+
+  def list_ordered_by_desc
+    Competition.order('created_at DESC');
+  end
+  
+  def finish(id)
+    Competition.update(id, :opened => false)
+  end
+
+  def create(params)
+    competition = Competition.new
+    competition.name = params["name"]
+    competition.discipline = @discipline
+    competition.save!
+    competition
+  end
+
+end
---
+++ app/repositories/discipline_repository.rb
@@ -0,0 +1,10 @@
+class DisciplineRepository
+  
+  def list_ordered_by_desc
+    Discipline.order('created_at DESC');
+  end
+
+  def find_by_slug(params)
+    Discipline.find_by!(slug: params)
+  end
+end
