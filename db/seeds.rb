2caf7b7119e6de1c410513ab2fec72518581a41f
Felipe Vieira <fpvieira@gmail.com>
2018-11-12 11:35:00 -0200

    Renomeia classe Rule

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Discipline.create(name: 'One Hundred Metre Dash', slug: 'one-hundred-metre-dash', rule_type: 'ShorterTimeRule')
+Discipline.create(name: 'One Hundred Metre Dash', slug: 'one-hundred-metre-dash', rule_type: 'ShorterTimeOnOneAttemptsRule')
 Discipline.create(name: 'Javelin Throw', slug: 'javelin-throw', rule_type: 'LongestThrownOnThreeAttemptsRule')
